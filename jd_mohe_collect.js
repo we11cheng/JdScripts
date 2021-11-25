8ed20e08403133c7224ff3286e446bf1d33aa85a
danwangshimoluo <evislcq@foxmail.com>
2021-11-25 14:24:59 +0800

    更新5g盲盒，拆分助力和做任务

--- jd_mohe_collect.js
+++ jd_mohe_collect.js
@@ -19,7 +19,7 @@ cron "0 0,1-23/3 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/s
 ====================================小火箭=============================
 5G超级盲盒 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_mohe.js, cronexpr="0 0,1-23/3 * * *", timeout=3600, enable=true
  */
-const $ = new Env('5G超级盲盒');
+const $ = new Env('5G超级盲盒收集能量值');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
