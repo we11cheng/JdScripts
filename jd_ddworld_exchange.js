49c199678515cfaccafb4de16db143d573aca012
danwangshimoluo <evislcq@foxmail.com>
2021-12-13 14:40:42 +0800

    更新京东图形签到

--- jd_ddworld_exchange.js
+++ jd_ddworld_exchange.js
@@ -61,7 +61,7 @@ async function main() {
             await task('get_exchange');
             if (!$.hotFlag) {
                 if ($.exchangeList) {
-                    for (const vo of $.exchangeList.reverse()) {
+                    for (const vo of $.exchangeList) {
                         $.log(`去兑换：${vo.name}`)
                         await taskPost('do_exchange', `id=${vo.id}`);
                         await $.wait(3000);
